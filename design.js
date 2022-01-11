0459ab2ef70df0909f4bf07d414c9c97af51cfc9
Okoli Chinedu Christian <Okolichukwunedu@gmail.com>
2022-01-11 22:22:38 +0100

    Update design.js

--- design.js
+++ design.js
@@ -4,6 +4,7 @@ const mysizePicker = document.getElementById('sizePicker');
 const mycolorPicker = document.getElementById('colorPicker');
 const mypixelCanvas = document.getElementById('pixelCanvas');
 
+
 // When size is submitted by the user, call makeGrid()
 mysizePicker.addEventListener ('submit', function (event) {
     mypixelCanvas.innerHTML = "";
@@ -13,21 +14,41 @@ mysizePicker.addEventListener ('submit', function (event) {
     makeGrid(width, height);
 });
 
-//To create the table from user's input
+
+/**
+* @desc create a grid of squares
+* @param int $width - number of squares representing the width of the
+grid
+* @param int $height - number of squares representing the height of the
+grid
+*/
 function makeGrid(width, height) {
       for (let i = 1; i <= height; i++) {
         let row = mypixelCanvas.insertRow();
         for (let j = 1; j <= width; j++) {
           let cell = row.insertCell();
-
-          // adding color and removing it in the cell
-          cell.addEventListener("click", function(event){
-              cell.style.background = mycolorPicker.value;
-          cell.addEventListener ("click", function(event){
-              event.preventDefault();
-              cell.style.backgroundColor = mycolorPicker.value;
-            })
-          })
+          cell.dataset.clicked = 'false';
         }
       }
 };
+
+
+// adding and removing color on the cell
+mypixelCanvas.addEventListener ("click", function(event){
+    let cell = null;
+    if (event.target.tagName.toLowerCase() == "td"){
+      cell = event.target;
+    }
+    if (cell){
+      console.log(cell.dataset.clicked);
+      if (cell.dataset.clicked == 'false'){
+        cell.style.background = mycolorPicker.value;
+        cell.dataset.clicked = true;
+    } else{
+      //Remove styling here
+      cell.style.background = 'none';
+      cell.dataset.clicked = 'false'
+    }
+  }
+
+});
